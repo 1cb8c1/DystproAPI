b6f56b1ee1e3a227863d0b2b18212a639585fadd
1cb8c1 <6art10miejm0ty1@gmail.com>
2020-12-08 20:30:36 +0100

    Refractored AuthorizationMiddleware

--- auth/AuthorizationMiddleware.js
+++ auth/AuthorizationMiddleware.js
@@ -1,32 +1,24 @@
+//IMPORTS
 const { userAuthorized } = require("../db/users");
 const { CODES } = require("../errors/Errors");
 
+//Functions
 const checkAuthorizationMiddleware = (role) => {
   return async (req, res, next) => {
-    try {
-      const user = req.user;
+    const user = req.user;
 
-      //verifyTokenMiddleware checks if user exists. No need to to that here.
-
-      const isAuthorized = await userAuthorized(user.user_id, role);
-      if (!isAuthorized) {
-        return res.status(403).send({
-          error: {
-            code: CODES.NOTAUTHORIZED,
-            message: "User not authorized",
-          },
-        });
-      }
-      next();
-    } catch (error) {
+    const isAuthorized = await userAuthorized(user.user_id, role);
+    if (!isAuthorized) {
       return res.status(403).send({
         error: {
           code: CODES.NOTAUTHORIZED,
-          message: "Failed to authorize",
+          message: "User not authorized",
         },
       });
     }
+    next();
   };
 };
 
+//Exports
 module.exports = { checkAuthorizationMiddleware };
