03cd54a920b4aea720dc1e1818a75c908d24bfd8
1cb8c1 <6art10miejm0ty1@gmail.com>
2020-12-08 10:46:06 +0100

    Added query for getting single driver

---
+++ db/dystprodb_sql/get_driver_by_id.sql
@@ -0,0 +1,7 @@
+CREATE FUNCTION get_driver_by_id(@driver_id INT, @distributor_ID INT)
+RETURNS TABLE
+AS
+RETURN
+SELECT driver_id, name, surname
+FROM drivers
+WHERE driver_id = @driver_id AND distributor_id = @distributor_id
