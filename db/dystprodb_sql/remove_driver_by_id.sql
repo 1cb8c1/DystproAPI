a5943ba748114aaafa5d87ba580c391345daf8cf
1cb8c1 <6art10miejm0ty1@gmail.com>
2020-12-08 10:03:58 +0100

    Changed remove driver procuedure

--- db/dystprodb_sql/remove_driver_by_id.sql
+++ db/dystprodb_sql/remove_driver_by_id.sql
@@ -1,6 +1,15 @@
 CREATE PROCEDURE remove_driver_by_id
-    @driver_id INT
+    @driver_id INT,
+    @distributor_id INT
 AS
 BEGIN
+    DECLARE @driversCount INT
+    SELECT @driversCount = COUNT(driver_id)
+    FROM drivers
+    WHERE driver_id = @driver_id AND distributor_id = @distributor_id
+
+    IF(@driversCount < 0)
+        THROW 50000, 'Trying to delete driver that doesnt exist for this distributor', 1
+
     DELETE drivers WHERE driver_id = @driver_id
 END
