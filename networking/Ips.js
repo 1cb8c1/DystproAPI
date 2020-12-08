06219332f150ba1218747d1d679598c3a42fb782
1cb8c1 <6art10miejm0ty1@gmail.com>
2020-12-08 14:29:09 +0100

    Refractored

--- networking/Ips.js
+++ networking/Ips.js
@@ -1,11 +1,14 @@
+//REMOVES PORT FROM IP STRING. WORKS FOR IPV4 AND IPV6
 const removePort = (ip) => {
   return ip.split(/((?::))(?:[0-9]+)$/gm)[0].replace(/\[(.*?)\]/g, "$1");
 };
 
+//COMPARES TWO IPS WITHOUT PORTS
 const compareIps = (ip1, ip2) => {
   return removePort(ip1) === removePort(ip2);
 };
 
+//EXPORTS
 module.exports = {
   removePort,
   compareIps,
