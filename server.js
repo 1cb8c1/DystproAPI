af317116d7bf74bd058f82d4553c3549b664ab0e
1cb8c1 <6art10miejm0ty1@gmail.com>
2020-12-08 14:26:26 +0100

    Refractoring and changing implementation

--- server.js
+++ server.js
@@ -1,41 +1,39 @@
+//IMPORTS
 const express = require("express");
-const { populatePoolsPromise } = require("./db/pools");
-const authRoute = require("./auth/AuthenticationRoute");
-const { verifyTokenMiddleware } = require("./auth/AuthenticationMiddleware");
-const {
-  checkAuthorizationMiddleware,
-} = require("./auth/AuthorizationMiddleware");
-const { ROLES } = require("./auth/Roles");
-const productsRoute = require("./products/ProductsRoute");
-const driversRoute = require("./drivers/DriversRoute");
 const CONFIG = require("./Config");
 const helmet = require("helmet");
 const pools = require("./db/pools");
+//ROUTES IMPORTS
+const authRoute = require("./auth/AuthenticationRoute");
+const productsRoute = require("./products/ProductsRoute");
+const driversRoute = require("./drivers/DriversRoute");
 
+//SETTING UP APP
 const app = express();
-app.use(helmet());
-app.set("trust proxy", true);
+app.use(helmet()); //Helps securing app
+app.set("trust proxy", true); //Trusting proxy provided by azure
 
 const listenHandler = app.listen(CONFIG.PORT, () => {
   console.log(`Listening on port ${CONFIG.PORT}...`);
 });
 
-const DBips = require("./db/ips");
-
-//Could add middleware to specific route!
+//ROOT
 app.get("/", async (req, res) => {
-  res.status(200).send("Hello world!");
+  res.status(200).send("Server is alive!");
 });
 
+//USING ROUTES
 app.use("/auth", authRoute);
 app.use("/products", productsRoute);
 app.use("/drivers", driversRoute);
 
+//GETAPP, USED TO GET INSTANCE IN TESTS
 const getApp = async () => {
-  await populatePoolsPromise;
+  await pools.populatePoolsPromise;
   return app;
 };
 
+//SHUTTING DOWN APP
 const shutDown = () => {
   console.log("Shutting down");
   listenHandler.close(() => {});
@@ -45,6 +43,7 @@ process.on("SIGINT", shutDown);
 process.on("SIGTERM", shutDown);
 process.on("uncaughtException", shutDown);
 
+//EXPORTS
 module.exports = {
   getApp,
   listenHandler,
