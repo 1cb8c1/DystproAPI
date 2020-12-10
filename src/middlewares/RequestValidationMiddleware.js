204879c7b321a2d6b2b55f769488af51f9f3399b
1cb8c1 <6art10miejm0ty1@gmail.com>
2020-12-10 21:33:26 +0100

    feat: Added Middleware for data validation in request

---
+++ src/middlewares/RequestValidationMiddleware.js
@@ -0,0 +1,30 @@
+//IMPORTS
+const { CODES } = require("../errors/Errors");
+
+//FUNCTIONS
+const requestValidationMiddleware = (schema, returnOnError = null) => {
+  return (req, res, next) => {
+    const { error } = schema.validate({
+      body: { ...req.body, ...req.params, ...req.query },
+    });
+    const valid = error == null;
+
+    if (valid) {
+      next();
+    } else {
+      const { details } = error;
+      const message = details.map((i) => i.message).join(",");
+
+      res.status(422).send({
+        error: {
+          code: CODES.BADARGUMENT,
+          message: message,
+        },
+        ...returnOnError,
+      });
+    }
+  };
+};
+
+//EXPORTS
+module.exports = requestValidationMiddleware;
