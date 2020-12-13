4cf558f7683d3f6781e17b838d0a08a5350d429b
1cb8c1 <6art10miejm0ty1@gmail.com>
2020-12-13 17:48:56 +0100

    feat!: Added requestValidationMiddleware to DriversRoute endpoints.
    BREAKING CHANGE: some code or messages responses may change.

--- src/routes/DriversRoute.js
+++ src/routes/DriversRoute.js
@@ -5,8 +5,10 @@ const bodyParser = require("body-parser");
 const DBdrivers = require("../models/drivers");
 const authorizationMiddleware = require("../middlewares/AuthorizationMiddleware");
 const authenticationMiddleware = require("../middlewares/AuthenticationMiddleware");
+const requestValidationMiddleware = require("../middlewares/RequestValidationMiddleware");
 const { ROLES } = require("../utils/auth/Roles");
 const sql = require("mssql");
+const schemas = require("../schemas/DriversSchemes");
 
 //SETTING UP ROUTER
 const router = express.Router();
@@ -28,89 +30,84 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-router.get("/:id", async (req, res, next) => {
-  try {
-    const driver = DBdrivers.getDriver(req.params.id, req.user.distributor);
-    return res.status(200).send({ driver: driver });
-  } catch (error) {
-    error.onResponseData = { driver: null };
-    return next(error);
-  }
-});
-
-router.post("/", async (req, res, next) => {
-  //CHECKING IF ARGUMENTS ARE CORRECT
-  try {
-    if (!Object.prototype.hasOwnProperty.call(req.body, "driver")) {
-      throw new Error("No driver found");
+router.get(
+  "/:id",
+  requestValidationMiddleware(schemas.driversGetSchema, { driver: null }),
+  async (req, res, next) => {
+    try {
+      const driver = DBdrivers.getDriver(req.params.id, req.user.distributor);
+      return res.status(200).send({ driver: driver });
+    } catch (error) {
+      error.onResponseData = { driver: null };
+      return next(error);
     }
-    if (!Object.prototype.hasOwnProperty.call(req.body.driver, "name")) {
-      throw new Error("No name found");
+  }
+);
+
+router.post(
+  "/",
+  requestValidationMiddleware(schemas.driversPostSchema, { driver: null }),
+  async (req, res, next) => {
+    //TRYING TO CREATE DRIVER
+    try {
+      const driver = await DBdrivers.addDriver(
+        req.body.driver.name,
+        req.body.driver.surname,
+        req.user.distributor
+      );
+
+      return res.status(200).send({ driver: driver });
+    } catch (error) {
+      error.onResponseData = { driver: null };
+      return next(error);
     }
-    if (!Object.prototype.hasOwnProperty.call(req.body.driver, "surname")) {
-      throw new Error("No surnamename found");
+  }
+);
+
+router.delete(
+  "/:id",
+  requestValidationMiddleware(schemas.driversDeleteSchema),
+  async (req, res, next) => {
+    //TRYING TO REMOVE DRIVER
+    try {
+      await DBdrivers.removeDriver(req.params.id, req.user.distributor);
+      return res.status(200).send({});
+    } catch (error) {
+      return next(error);
     }
-  } catch (error) {
-    return res.status(400).send({
-      error: {
-        code: CODES.BADARGUMENT,
-        message: error.message,
-      },
-      driver: null,
-    });
   }
+);
 
-  //TRYING TO CREATE DRIVER
-  try {
-    const driver = await DBdrivers.addDriver(
-      req.body.driver.name,
-      req.body.driver.surname,
-      req.user.distributor
-    );
+router.patch(
+  "/:id",
+  requestValidationMiddleware(schemas.driversPatchSchema, { driver: null }),
+  async (req, res, next) => {
+    //TRYING TO MODIFY DRIVER
+    try {
+      await DBdrivers.modifyDriver(
+        req.params.id,
+        req.body.driver.name,
+        req.body.driver.surname,
+        req.user.distributor
+      );
+    } catch (error) {
+      error.onResponseData = { driver: null };
+      return next(error);
+    }
 
-    return res.status(200).send({ driver: driver });
-  } catch (error) {
-    error.onResponseData = { driver: null };
-    return next(error);
+    //TRYING TO GET MODIFIED DRIVER
+    try {
+      const driver = await DBdrivers.getDriver(
+        req.params.id,
+        req.user.distributor
+      );
+      return res.status(200).send({ driver: driver });
+    } catch (error) {
+      error.onResponseData = { driver: null };
+      return next(error);
+    }
   }
-});
-
-router.delete("/:id", async (req, res, next) => {
-  //TRYING TO REMOVE DRIVER
-  try {
-    await DBdrivers.removeDriver(req.params.id, req.user.distributor);
-    return res.status(200).send({});
-  } catch (error) {
-    return next(error);
-  }
-});
-
-router.patch("/:id", async (req, res, next) => {
-  //TRYING TO MODIFY DRIVER
-  try {
-    await DBdrivers.modifyDriver(
-      req.params.id,
-      req.body.driver.name,
-      req.body.driver.surname,
-      req.user.distributor
-    );
-  } catch (error) {
-    error.onResponseData = { driver: null };
-    return next(error);
-  }
-
-  //TRYING TO GET MODIFIED DRIVER
-  try {
-    const driver = await DBdrivers.getDriver(
-      req.params.id,
-      req.user.distributor
-    );
-    return res.status(200).send({ driver: driver });
-  } catch (error) {
-    error.onResponseData = { driver: null };
-    return next(error);
-  }
-});
+);
 
 //EXPORTS
 module.exports = router;
