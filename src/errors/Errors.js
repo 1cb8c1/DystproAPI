fde4e67610d970533f4e2491202c67fa66eb48e6
1cb8c1 <6art10miejm0ty1@gmail.com>
2020-12-13 16:49:36 +0100

    feat: Added attributes CODE and STATUS to DATABASE_ERRORS and added two objects to DATABASE_ERRORS

--- src/errors/Errors.js
+++ src/errors/Errors.js
@@ -5,6 +5,7 @@ const CODES = {
   LOGIC: "LOGIC",
   NOTAUTHORIZED: "NOTAUTHORIZED",
   REJECTED: "REJECTED",
+  NOTFOUND: "NOT FOUND",
 };
 
 const DATABASE_ERRORS = {
@@ -12,20 +13,41 @@ const DATABASE_ERRORS = {
     MESSAGE: "Driver doesn't exist",
     DEVELOPER_INFO:
       "User tried to delete driver that doesn't exist or is out of scope of his distributor (belongs to different distributor)",
+    CODE: CODES.NOTFOUND,
+    STATUS: 404,
   },
   50001: {
     MESSAGE: "Driver doesn't exist",
     DEVELOPER_INFO:
       "User tried to modify driver that doesn't exist or is out of scope of his distributor (belongs to different distributor)",
+    CODE: CODES.NOTFOUND,
+    STATUS: 404,
   },
   50002: {
     MESSAGE: "Too many requests",
     DEVELOPER_INFO: "User exceeded number of requests",
+    CODE: CODES.REJECTED,
+    STATUS: 409,
   },
   50003: {
     MESSAGE: "Request doesn't exist",
     DEVELOPER_INFO:
       "User tried to delete request that doesn't exist or is outside of his scope",
+    CODE: CODES.NOTFOUND,
+    STATUS: 404,
+  },
+  50004: {
+    MESSAGE: "Vechicle with such registration number already exists",
+    DEVELOPER_INFO: "User tried to add vechicle that already exists",
+    CODE: CODES.REJECTED,
+    STATUS: 422,
+  },
+  50005: {
+    MESSAGE: "Vechicle doesn't exist",
+    DEVELOPER_INFO:
+      "User tried to delete vechicle that doesn't exist or is outside of his scope",
+    CODE: CODES.NOTFOUND,
+    STATUS: 404,
   },
 };
 
