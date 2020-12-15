18a1cd68647974ca61cefaeaa88123e75cad00d8
1cb8c1 <6art10miejm0ty1@gmail.com>
2020-12-15 14:30:14 +0100

    test: Added tests for distributors model

---
+++ tests/db.distributors.test.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+jest.setTimeout(10000);
+const { getApp, shutDown } = require("../server");
+const DBdistributors = require("../src/models/distributors");
+
+//REGISTER
+describe("DB products", () => {
+  it("should reserve and cancel reservation", async () => {
+    await getApp();
+
+    const distributorId = 1;
+    const discount = await DBdistributors.getDistributorDiscount(distributorId);
+    expect(discount).toBeCloseTo(5.0);
+  });
+});
+
+//Fix closing down during tests!
+//Not it uses --forceExit
+afterAll(() => {
+  shutDown();
+});
