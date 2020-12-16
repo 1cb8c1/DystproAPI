6d7f38e164ac7aebb9bffb083d3b0f0d61f4b279
1cb8c1 <6art10miejm0ty1@gmail.com>
2020-12-16 18:44:32 +0100

    feat: Added errors that might be returned by procedures: create_reservation, cancel_reservation

--- src/errors/Errors.js
+++ src/errors/Errors.js
@@ -49,6 +49,62 @@ const DATABASE_ERRORS = {
     CODE: CODES.NOTFOUND,
     STATUS: 404,
   },
+  50006: {
+    MESSAGE: "Trying to reserve too small amount (0)",
+    DEVELOPER_INFO: "User tried to reserve amount of product that is 0",
+    CODE: CODES.REJECTED,
+    STATUS: 422,
+  },
+  50007: {
+    MESSAGE:
+      "Trying to reserve amount that exceedes amount that is available in this warehouse",
+    DEVELOPER_INFO:
+      "User tried to reserve amount of producta that exceedes available amount in the warehouse",
+    CODE: CODES.REJECTED,
+    STATUS: 422,
+  },
+  50008: {
+    MESSAGE: "Failed to create reservation",
+    DEVELOPER_INFO:
+      "Failed to create reservation, it shouldn't happen as checks has been made to ensure that it will be made. Failes when created reservation count is != 1",
+    CODE: CODES.LOGIC,
+    STATUS: 500,
+  },
+  50009: {
+    MESSAGE: "Failed to update products_warehouses",
+    DEVELOPER_INFO:
+      "Failed to update one row in producs_warehouses, it shouldn't happen since there is xlock on that row",
+    CODE: CODES.LOGIC,
+    STATUS: 500,
+  },
+  50010: {
+    MESSAGE: "Reservation doesn't exist",
+    DEFELOPER_INFO:
+      "User tried to delete reservation that doesn't exist, or there are two reservations with the same id (shouldn't happen)",
+    CODE: CODES.NOTFOUND,
+    STATUS: 404,
+  },
+  50011: {
+    MESSAGE: "Product in such warehouse doesn't exist",
+    DEVELOPER_INFO:
+      "Reservation had product_warehouse_id that doesn't exist in table products_warehouses. This shouldn't happen, as all products_warehouses are archived",
+    CODE: CODES.LOGIC,
+    STATUS: 500,
+  },
+  50012: {
+    MESSAGE: "Failed to delete one reservation",
+    DEVELOPER_INFO:
+      "Failed to delete reservation (shouldn't happen since there is xlock on the row reservations), or deleted more than one reservation (shouldn't happen)",
+    CODE: CODES.LOGIC,
+    STATUS: 500,
+  },
+  50013: {
+    MESSAGE: "Failed to update product in a warehouse",
+    DEVELOPER_INFO:
+      "Failed to update product_warehouse (amount), This shouldn't happen since there is xlock on that product_warehouse",
+    CODE: CODES.LOGIC,
+    STATUS: 500,
+  },
 };
 
 //EXPORTS
