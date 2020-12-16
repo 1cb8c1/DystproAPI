70f36972d106bbac0e192f6c1e555a0ef9e21047
1cb8c1 <6art10miejm0ty1@gmail.com>
2020-12-16 18:40:42 +0100

    feat: Added t-sql query for creating reservation

---
+++ src/db/mssql_queries/create_reservation.sql
@@ -0,0 +1,63 @@
+CREATE PROCEDURE create_reservation
+    @distributor_id INT,
+    @product_warehouse_id INT,
+    @amount INT,
+    @reservation_id INT OUTPUT
+AS
+SET XACT_ABORT ON
+BEGIN
+    SET TRANSACTION ISOLATION LEVEL SERIALIZABLE
+    BEGIN TRANSACTION
+    IF(@amount < 1)
+        THROW 50006, 'Trying to reserve amount that is too small (0)', 1
+
+
+    DECLARE @availableAmount INT
+    DECLARE @product_id INT
+
+    SELECT @availableAmount = amount, @product_id = product_id
+    FROM products_warehouses WITH (XLOCK, ROWLOCK)
+    WHERE product_warehouse_id = @product_warehouse_id
+
+    IF(@amount > @availableAmount)
+        THROW 50007, 'Trying to reserve amount that is too big (not available)', 1
+
+
+
+    DECLARE @price MONEY
+    SELECT @price = price
+    FROM products
+    WHERE product_id = @product_id
+
+    DECLARE @discount FLOAT
+    SELECT @discount = discount
+    FROM distributors
+    WHERE distributor_id = @distributor_id
+
+    DECLARE @my_table table(reservation_id INT)
+
+    INSERT reservations
+        (distributor_id, product_warehouse_id, amount, reservation_date, price)
+    OUTPUT INSERTED.reservation_id INTO @my_table
+    VALUES(@distributor_id, @product_warehouse_id, @amount, GETDATE(), @price * @amount - (@price * @amount * @discount / 100.0))
+
+    IF((SELECT COUNT(reservation_id)
+    FROM @my_table) != 1)
+         THROW 50008, 'Failed to create one reservation', 1
+
+    SELECT @reservation_id = reservation_id
+    FROM @my_table
+
+
+    DECLARE @my_table2 table(product_warehouse_id INT)
+
+    UPDATE products_warehouses SET amount = @availableAmount - @amount 
+    OUTPUT INSERTED.product_warehouse_id INTO @my_table2
+    WHERE product_warehouse_id = @product_warehouse_id
+
+    IF((SELECT COUNT(product_warehouse_id)
+    FROM @my_table2) != 1)
+        THROW 50009, 'Failed to update one product_warehouse', 1
+
+    COMMIT TRANSACTION
+END
