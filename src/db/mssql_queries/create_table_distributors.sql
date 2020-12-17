e016df4fab4790ff33178cf7d8718397584ab530
1cb8c1 <6art10miejm0ty1@gmail.com>
2020-12-17 13:29:24 +0100

    fix!: Changed data types in fields name in distributors table.
    BREAKING CHANGE: Data type changed from varchar to nvarchar

--- src/db/mssql_queries/create_table_distributors.sql
+++ src/db/mssql_queries/create_table_distributors.sql
@@ -1,7 +1,7 @@
 CREATE TABLE distributors
 (
     distributor_id INT IDENTITY(1,1) NOT NULL,
-    name VARCHAR(32) NOT NULL UNIQUE,
+    name NVARCHAR(32) NOT NULL UNIQUE,
     discount float,
     archived BIT NOT NULL DEFAULT 0,
     PRIMARY KEY(distributor_id)
