b30404cf284607deeb1779871ef33afe19ceef8e
1cb8c1 <6art10miejm0ty1@gmail.com>
2020-12-17 18:55:55 +0100

    fix: Product_warehouse that is archived, can't be reserved

--- src/db/mssql_queries/create_reservation.sql
+++ src/db/mssql_queries/create_reservation.sql
@@ -12,22 +12,33 @@ BEGIN
         THROW 50006, 'Trying to reserve amount that is too small (0)', 1
 
 
+    /*CHECKS IF PRODUCT_WAREHOUSE EXISTS AND IF IS NOT ARCHIVED*/
+    DECLARE @products_warehouses_count INT
+    SELECT @products_warehouses_count = COUNT(product_warehouse_id)
+    FROM products_warehouses 
+    WITH(XLOCK, ROWLOCK)
+    WHERE product_warehouse_id = @product_warehouse_id AND archived = 0
+    IF(@products_warehouses_count != 1)
+        THROW 50011, 'Number of product_warehouse with such id is not one', 1
+
+
     DECLARE @availableAmount INT
     DECLARE @product_id INT
 
     SELECT @availableAmount = amount, @product_id = product_id
     FROM products_warehouses WITH (XLOCK, ROWLOCK)
-    WHERE product_warehouse_id = @product_warehouse_id
+    WHERE product_warehouse_id = @product_warehouse_id AND archived = 0
 
     IF(@amount > @availableAmount)
         THROW 50007, 'Trying to reserve amount that is too big (not available)', 1
 
 
 
+    /*IF product IS ARCHIVED, SO SHOULD product_warehouse BE. BUT CHECKING ANYWAYS*/
     DECLARE @price MONEY
     SELECT @price = price
     FROM products
-    WHERE product_id = @product_id
+    WHERE product_id = @product_id AND archived = 0
 
     DECLARE @discount FLOAT
     SELECT @discount = discount
@@ -53,7 +64,7 @@ BEGIN
 
     UPDATE products_warehouses SET amount = @availableAmount - @amount 
     OUTPUT INSERTED.product_warehouse_id INTO @my_table2
-    WHERE product_warehouse_id = @product_warehouse_id
+    WHERE product_warehouse_id = @product_warehouse_id AND archived = 0
 
     IF((SELECT COUNT(product_warehouse_id)
     FROM @my_table2) != 1)
